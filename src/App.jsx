9cc3e5389b5e7bc921833f9410476b1aece5572e
Hossam Ramadan <103776358+HossamRamadan667@users.noreply.github.com>
2024-07-21 20:33:57 +0300

    prepare react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
+
 function App() {
-	return <div className=" "></div>;
+	return (
+		<>
+			<Header />
+			<Routes>
+				<Route path="/" element={<div>home</div>} /> 
+			</Routes>
+		</>
+	);
 }
 
 export default App;
